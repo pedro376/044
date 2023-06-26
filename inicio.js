314241ea4153a2a0e39ed8a45472471c0a557a54
pedro376 <137464398+pedro376@users.noreply.github.com>
2023-06-26 10:51:41 -0700

    pop
    prueba definitiva

---
+++ inicio.js
@@ -0,0 +1,12 @@
+var check=document.querySelector(".check");
+check.addEventListener('click',idioma);
+
+function idioma(){
+    let id=check.checked
+    if (id==true){
+        location.href="index.html"
+    }
+    else{
+        location.href="main_page.html"
+    }
+}
